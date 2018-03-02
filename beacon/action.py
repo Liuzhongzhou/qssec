caa803a0f242274c3962dd3024fa2dfbfb05025a
du8108 <81084935@qq.com>
2018-03-02 16:23:24 +0800

    格式化

--- beacon/action.py
+++ beacon/action.py
@@ -3,5 +3,5 @@
 from beacon_user.views import \
     user_list, user_info, user_save, user_delete, \
     role_list, role_info, role_save, role_delete, \
-    organization_list,organization_delete,organization_save,\
+    organization_list, organization_delete, organization_save, \
     menu_list, my_menu
