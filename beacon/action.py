84fde96851ed4d5b0ace0b09d6fb13abe5e56ced
刘中州 <30714046+Liuzhongzhou@users.noreply.github.com>
2018-03-06 17:01:46 +0800

    应用系统的查询，增加，修改，删除操作

--- beacon/action.py
+++ beacon/action.py
@@ -4,12 +4,7 @@ from beacon_user.views import \
     user_list, user_info, user_save, user_delete, \
     role_list, role_info, role_save, role_delete, \
     organization_list, organization_delete, organization_save, \
-<<<<<<< Updated upstream
     menu_list, my_menu,city_list
-=======
-    menu_list, my_menu
 
-from beacon_app.views import app_list, appsys_save,appsys_delete
+from beacon_app.views import *
 
-
->>>>>>> Stashed changes
